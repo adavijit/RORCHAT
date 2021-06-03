d9912a511ed8a488d296b7fcfab9df881b448600
Avijit Das <adavijit@yahoo.com>
2021-06-03 15:42:58 +0530

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root "home#index"
+
+  get '/login', to: 'login#index'
+
+  get '/signup', to: 'signup#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
